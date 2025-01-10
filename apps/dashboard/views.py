ea296ad517ac53ec5fc972d353c98c6f4f2e9888
DataRohit <rohit.vilas.ingole@gmail.com>
2025-01-10 16:55:58 +0530

    feature: dashboard template update & row height increased

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,6 +2,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from apps.dashboard.models import StockIndexWatchlist
+from apps.socket.helpers import get_quote
+
 
 # Dashboard Home View
 @login_required
@@ -15,8 +18,27 @@ def home_view(request):
         HttpResponse: The response object
     """
 
+    # Fetch the bookmarked indices and stocks
+    bookmarked_items = StockIndexWatchlist.objects.filter(user=request.user).order_by(
+        "-type", "symbol"
+    )
+
+    # Dict to store the quotes
+    quotes = {}
+
+    # Traverse over the bookmarked items
+    for item in bookmarked_items:
+        # Get the quote for the item
+        quote = get_quote(item.symbol)
+
+        # Append the quote to the quotes dict
+        quotes[quote[0]] = quote[1]
+
     # Create a context dictionary
-    context = {"user": request.user}
+    context = {
+        "user": request.user,
+        "quotes": quotes,
+    }
 
     # Render the dashboard.html template
     return render(request, "dashboard/dashboard.html", context)
